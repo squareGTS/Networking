38162387bf5ad46a0b720dc0c5f9cc7f2be42aec
Maxim <68609996+squareGTS@users.noreply.github.com>
2020-12-24 16:36:44 +0300

    Create singelton session.

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -8,14 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var getImageButton: UIButton!
-
-    @IBAction func getImagePressed(_ sender: Any) {
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
     }
     
+    @IBAction func getImagePressed(_ sender: Any) {
+        
+        label.isHidden = true
+        getImageButton.isEnabled = false
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg")
+        else { return }
+        
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
+    }
 }
-
