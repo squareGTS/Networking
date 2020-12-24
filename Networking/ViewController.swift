b5e910695a55f68bccc9241102084f5e82b3c4d7
Maxim <68609996+squareGTS@users.noreply.github.com>
2020-12-24 16:05:19 +0300

    Initial Commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Alexey Efimov on 25/07/2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var getImageButton: UIButton!
+
+    @IBAction func getImagePressed(_ sender: Any) {
+    }
+    
+}
+
